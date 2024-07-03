93258aed73a494dedaa509a3007b8214e3625ed7
salman261002 <salmanalfarisi261002@gmail.com>
2024-07-03 14:06:27 +0700

    feat: update

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -11,7 +11,7 @@ export function GET(request: Request) {
           tw="absolute inset-0 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_20%,transparent_40%,#63e_100%)]"
           style={{
             background:
-              "radial-gradient(125% 125% at 50% 20%, transparent 40%, #63e 100%)",
+              "radial-gradient(125% 125% at 50% 20%, #fff 40%, #63e 100%)",
           }}
         />
         <div tw="flex flex-col w-full py-12 px-4 items-center justify-between">
