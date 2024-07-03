727bbf9ee10c3ce5dbf4ca5e5db1d08b63e7adea
salman261002 <salmanalfarisi261002@gmail.com>
2024-07-03 13:51:44 +0700

    feat: update

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,9 +6,9 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white relative">
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-transparent relative">
         <div tw="absolute inset-0 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_20%,transparent_40%,#63e_100%)]"></div>
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+        <div tw="flex flex-col w-full py-12 px-4 items-center justify-between">
           <h2 tw="text-4xl font-bold text-center">{title}</h2>
         </div>
       </div>
