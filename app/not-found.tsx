79b6c22c0f12453c1a9bd94f1b3e2c86a9837219
salman261002 <salmanalfarisi261002@gmail.com>
2024-10-19 03:59:26 +0700

    feat: add not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Button from "@/components/shared/button";
+
 const NotFound = () => {
   return (
-    <div className="flex h-auto grow items-center justify-center">
-      Not found
+    <div className="flex h-auto grow flex-col items-center justify-center text-foreground">
+      <h1 className="text-xl md:text-2xl font-semibold mb-4">404</h1>
     </div>
   );
 };
